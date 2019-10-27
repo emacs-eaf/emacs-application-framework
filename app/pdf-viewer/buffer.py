8b0ce36d95730356ecb667a94d84e5d2e26ad0e6
luhuaei <luhuaei@gmail.com>
2019-10-27 11:47:07 +0800

    Fix: if display two pages, don't work on first page.

--- app/pdf-viewer/buffer.py
+++ app/pdf-viewer/buffer.py
@@ -422,10 +422,11 @@ class PdfViewerWidget(QWidget):
 
                 # computer absolute coordinate of page
                 x = int((pos.x() - render_x) * 1.0 / self.scale)
-                if last_page_index - start_page_index == 1:
+                if pos.y() + self.scroll_offset < (start_page_index + 1) * self.scale * self.page_height:
                     page_offset = self.scroll_offset - start_page_index * self.scale * self.page_height
                 else:
-                    page_offset = self.scroll_offset - (start_page_index + 1) * self.scale * self.page_height
+                    # if display two pages, pos.y() will add page_padding
+                    page_offset = self.scroll_offset - (start_page_index + 1) * self.scale * self.page_height - self.page_padding
                 y = int((pos.y() + page_offset) * 1.0 / self.scale)
                 word_offset = 10 # 10 pixel is enough for word intersect operation
                 draw_rect = fitz.Rect(x, y, x + word_offset, y + word_offset)
