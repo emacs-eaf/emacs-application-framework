d893b9c721d72f25d181bca40301ba49dc67ea23
Matthew <matthewzmd@gmail.com>
2019-12-18 23:12:48 -0500

    Use file-name-nondirectory instead of regex hack

--- eaf.el
+++ eaf.el
@@ -551,8 +551,8 @@ to edit EAF keybindings!" fun fun)))
 (defun eaf--create-buffer (input-content app-name)
   "Create an EAF buffer given INPUT-CONTENT and APP-NAME."
   (eaf--gen-keybinding-map (eaf--get-app-bindings app-name))
-  (let* ((file-or-command-name (substring input-content (string-match "[^/]*/?$" input-content)))
-         (eaf-buffer (generate-new-buffer (truncate-string-to-width file-or-command-name eaf-title-length))))
+  (let* ((input-content (if (file-exists-p input-content) (file-name-nondirectory input-content) input-content))
+         (eaf-buffer (generate-new-buffer (truncate-string-to-width input-content eaf-title-length))))
     (with-current-buffer eaf-buffer
       (eaf-mode)
       (set (make-local-variable 'eaf--buffer-url) input-content)
