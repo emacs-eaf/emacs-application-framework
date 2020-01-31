aa7c8c949c5b820985411f9c2fc0ebdaff6dd854
Andy Stewart <lazycat.manatee@gmail.com>
2020-01-31 10:50:02 +0800

    Add comment.

--- core/js/set_focus_text.js
+++ core/js/set_focus_text.js
@@ -3,6 +3,8 @@
     const activeElement = document.activeElement;
     activeElement.value = decodeURIComponent(escape(window.atob(newText)));
 
+    // Note: simulate click event on active element after set focus text.
+    // Some website need click input element before submit form.
     let clickEvent = document.createEvent('Events');
     clickEvent.initEvent("click", true, false);
     activeElement.dispatchEvent(clickEvent);
