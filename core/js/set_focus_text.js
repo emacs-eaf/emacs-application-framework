cae17fdabce5f51922b79af22624077e2df9e92f
Andy Stewart <lazycat.manatee@gmail.com>
2020-01-31 10:48:37 +0800

    Click active element after set focus text.

--- core/js/set_focus_text.js
+++ core/js/set_focus_text.js
@@ -2,4 +2,8 @@
     let newText = "%1";
     const activeElement = document.activeElement;
     activeElement.value = decodeURIComponent(escape(window.atob(newText)));
+
+    let clickEvent = document.createEvent('Events');
+    clickEvent.initEvent("click", true, false);
+    activeElement.dispatchEvent(clickEvent);
 })();
