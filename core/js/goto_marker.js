f797fca69d411a0ca926e8f684607bd8389a4560
Andy Stewart <lazycat.manatee@gmail.com>
2020-02-02 09:53:54 +0800

    We ned move cursor to end of input after focus input.

--- core/js/goto_marker.js
+++ core/js/goto_marker.js
@@ -8,17 +8,32 @@
             break;
         }
     }
+
+    function moveCursorToEnd(el) {
+        if (typeof el.selectionStart == "number") {
+            el.selectionStart = el.selectionEnd = el.value.length;
+        } else if (typeof el.createTextRange != "undefined") {
+            el.focus();
+            var range = el.createTextRange();
+            range.collapse(false);
+            range.select();
+        }
+    }
+
     if(match != undefined){
         let selector = match.getAttribute('pointed-link');
         let link = document.querySelector(selector);
-        if(link.nodeName.toLowerCase() === 'input'){
+        if(link.nodeName.toLowerCase() === 'input' ||
+           link.nodeName.toLowerCase() === 'textarea') {
             if(link.getAttribute('type') === 'submit'){
                 link.submit();
             } else if(link.getAttribute('type') === 'checkbox'){
                 link.click();
             } else {
                 link.focus();
-                link.select();
+
+                // We ned move cursor to end of input after focus input.
+                moveCursorToEnd(link);
             }
         } else if((link.nodeName.toLowerCase() === 'button') || // normal button
                   (link.hasAttribute('aria-haspopup')) || // menu button
@@ -26,9 +41,6 @@
                   (link.classList.contains('btn')) || // class="btn" buttons
                   (link.getAttribute('href') === '')){ // special href case that's button
             link.click();
-        } else if(link.nodeName.toLowerCase() === 'textarea'){
-            link.focus();
-            link.select();
         } else if(link.href != undefined && link.href != '' && link.getAttribute('href') != ''){
             return link.href;
         }
