1a1ba0d412a2c9c98da12c66a4829a4b77eba47a
Andy Stewart <lazycat.manatee@gmail.com>
2020-02-26 13:41:51 +0800

    Use deletechar instead M-o

--- eaf.el
+++ eaf.el
@@ -420,7 +420,7 @@ Try not to modify this alist directly.  Use `eaf-setq' to modify instead."
 
 (defcustom eaf-mindmap-keybinding
   '(("TAB" . "add_sub_node")
-    ("M-o" . "remove_node")
+    ("<deletechar>" . "remove_node")
     ("M-m" . "update_node_topic")
     ("M-r" . "refresh_page")
     ("C--" . "zoom_out")
@@ -1300,7 +1300,7 @@ This function works best if paired with a fuzzy search package."
                    (if history-file-exists
                        (mapcar
                         (lambda (h) (when (string-match history-pattern h)
-                                      (format "[%s] ⇰ %s" (match-string 1 h) (match-string 2 h))))
+                                  (format "[%s] ⇰ %s" (match-string 1 h) (match-string 2 h))))
                         (with-temp-buffer (insert-file-contents browser-history-file-path)
                                           (split-string (buffer-string) "\n" t)))
                      nil)))
