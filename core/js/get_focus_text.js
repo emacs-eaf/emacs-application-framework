143ad1a23f47cce34dae8207d6515e44527d13bb
Andy Stewart <lazycat.manatee@gmail.com>
2020-03-22 23:18:17 +0800

    Fix #253

--- core/js/get_focus_text.js
+++ core/js/get_focus_text.js
@@ -2,7 +2,11 @@
     const activeElement = document.activeElement;
     var inputs = ["input", "select", "textarea"];
 
-    if (activeElement && inputs.indexOf(activeElement.tagName.toLowerCase()) !== -1) {
+    if (window.location.href === "https://mail.qq.com/" && activeElement) {
+        // QQ mail have some security mechanism that we can't fetch value of activeElement.
+        // So we just return empty string make is_focus method works well in browser.py
+        return "";
+    } else if (activeElement && inputs.indexOf(activeElement.tagName.toLowerCase()) !== -1) {
         return activeElement.value;
     } else {
         if (window.location.href.startsWith("https://web.telegram.org/") && activeElement.hasAttribute("placeholder")) {
