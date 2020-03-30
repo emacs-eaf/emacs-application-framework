39af3b7f3c8ca5819f2e974191991a7662e3468c
Andy Stewart <lazycat.manatee@gmail.com>
2020-03-30 12:01:25 +0800

    Fix aria2 duplicate download issue.

--- core/browser.py
+++ core/browser.py
@@ -681,8 +681,10 @@ class BrowserBuffer(Buffer):
             with open(os.devnull, "w") as null_file:
                 aria2_args = ["aria2c"]
 
-                aria2_args.append("-d")
-                aria2_args.append(os.path.expanduser(str(self.emacs_var_dict["eaf-browser-download-path"])))
+                aria2_args.append("-d") # daemon
+                aria2_args.append("-c") # continue download
+                aria2_args.append("--auto-file-renaming=false") # not auto rename file
+                aria2_args.append("-d {}".format(os.path.expanduser(str(self.emacs_var_dict["eaf-browser-download-path"]))))
 
                 aria2_proxy_host = str(self.emacs_var_dict["eaf-browser-aria2-proxy-host"])
                 aria2_proxy_port = str(self.emacs_var_dict["eaf-browser-aria2-proxy-port"])
