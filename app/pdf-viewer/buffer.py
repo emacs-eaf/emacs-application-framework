d9e00126b16f4832e6d790d6b58a8262233edddd
luhuaei <luhuaei@gmail.com>
2020-04-11 12:50:59 +0800

    pdf-viewer: fix display on right when read_mode is fit_to_height

--- app/pdf-viewer/buffer.py
+++ app/pdf-viewer/buffer.py
@@ -387,8 +387,9 @@ class PdfViewerWidget(QWidget):
                     painter.translate(0, self.page_padding)
 
                 # Draw page image.
-                visible_x = max(min(render_x + self.horizontal_offset, 0), self.rect().width() - render_width) # limit the visiable area size
-                painter.drawPixmap(QRect(visible_x, render_y, render_width, render_height), qpixmap)
+                if self.read_mode == "fit_to_customize":
+                    render_x = max(min(render_x + self.horizontal_offset, 0), self.rect().width() - render_width) # limit the visiable area size
+                painter.drawPixmap(QRect(render_x, render_y, render_width, render_height), qpixmap)
 
         # Clean unused pixmap cache that avoid use too much memory.
         self.clean_unused_page_cache_pixmap()
