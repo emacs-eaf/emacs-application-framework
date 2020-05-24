bac840d2f293e9c1ee9b9fdca40fcefa7d421eb5
stardiviner <numbchild@gmail.com>
2020-05-24 09:50:50 +0800

    create new file eaf-org.el

---
+++ eaf-org.el
@@ -0,0 +1,43 @@
+;; eaf-org.el --- Emacs application framework  -*- lexical-binding: t; -*-
+
+;; Filename: eaf-org.el
+;; Description: Emacs application framework
+;; Author:  stardiviner <numbchild@gmail.com>
+;; Maintainer: Andy Stewart <lazycat.manatee@gmail.com>
+;; Copyright (C) 2018, Andy Stewart, all rights reserved.
+;; Created: 2020-05-17 12:31:12
+;; Version: 0.5
+;; Last-Updated: Wed May 20 11:48:43 2020 (-0400)
+;;           By: Mingde (Matthew) Zeng
+;; URL: http://www.emacswiki.org/emacs/download/eaf.el
+;; Keywords:
+;; Compatibility: GNU Emacs 27.0.50
+;;
+;; Features that might be required by this library:
+;;
+;; Please check README
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+
+
+
+(provide 'eaf-org)
