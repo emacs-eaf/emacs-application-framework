ea8295dff0c4ab721da260ab40f629e48b83b6c2
zbelial <zjytj@qq.com>
2020-09-12 12:09:27 +0800

    rename total_page to page_total_number

--- app/pdf-viewer/buffer.py
+++ app/pdf-viewer/buffer.py
@@ -147,7 +147,7 @@ class AppBuffer(Buffer):
         else:
             self.message_to_emacs.emit("Cannot copy, you should double click your mouse and hover through the text on the PDF. Don't click and drag!")
 
-    def total_page(self):
+    def page_total_number(self):
         return str(self.buffer_widget.page_total_number)
 
     def current_page(self):
