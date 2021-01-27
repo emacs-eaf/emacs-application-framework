60094faed969fd70726dec9b9e0afbe414be8a78
vconcat <imskyee@gmail.com>
2021-01-27 09:14:51 +0800

    update install script for Windows

--- install-eaf-win32.js
+++ install-eaf-win32.js
@@ -5,6 +5,7 @@ const https = require("https");
 const fs = require("fs");
 const crypto = require("crypto");
 const os = require("os");
+const url = require("url");
 
 const nodeApps = ["js-video-player", "mermaid", "terminal"];
 
@@ -40,34 +41,51 @@ function installPythonDep() {
     });
 }
 
-const kliteCodecPackURL = "https://files3.codecguide.com/K-Lite_Codec_Pack_1595_Basic.exe";
-const kliteCodecPackHash = "73772a2a0e8f9bc4ec4ca35578608cda76847e563613d1600bd5bf659a908559";
-
-function downloadFile(url) {
+function downloadFile(urlStr, sha256hash) {
+    const u = url.parse(urlStr);
     let handler = http;
-    if (url.startsWith("https://")) {
+    if (u.protocol === "https:") {
         handler = https;
     }
-    const name = crypto.randomBytes(4).readUInt32LE(0) + ".exe";
+    const hasher = crypto.createHash("sha256");
+    const name = path.posix.basename(u.pathname);
     const file = path.join(os.tmpdir(), name);
     return new Promise((resolve, reject) => {
-        handler.get(url, (res) => {
-            let data = [];
-            res.on("data", (chunk) => {
-                data.push(chunk);
+        fs.open(file, "w", (err, fd) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            handler.get(u, (res) => {
+                res.on("data", (chunk) => {
+                    fs.write(fd, chunk, (err) => {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+                    });
+                    hasher.update(chunk);
+                });
+                res.on("end", () => {
+                    const hash = hasher.digest("hex");
+                    if (hash !== sha256hash) {
+                        reject(new Error("download failed: hash mismatch"));
+                        return
+                    }
+                    resolve(file);
+                });
+            }).on("error", (err) => {
+                reject(err)
             });
-            res.on("end", () => {
-                fs.writeFileSync(file, Buffer.concat(data));
-                resolve(file);
-            });
-        }).on("error", (err) => {
-            reject(err)
         });
     });
 }
 
+const kliteCodecPackURL = "https://files3.codecguide.com/K-Lite_Codec_Pack_1595_Basic.exe";
+const kliteCodecPackHash = "73772a2a0e8f9bc4ec4ca35578608cda76847e563613d1600bd5bf659a908559";
+
 async function installCodecPack() {
-    const installer = await downloadFile(kliteCodecPackURL);
+    const installer = await downloadFile(kliteCodecPackURL, kliteCodecPackHash);
     const installCmd = `${installer} /verysilent /nocancel /norestart`;
     console.log("installing k-lite-codec-pack basic ...");
     execSync(installCmd);
@@ -79,7 +97,7 @@ installPythonDep();
 
 (async function() {
     try {
-    await installCodecPack();
+        await installCodecPack();
     } catch (err) {
         console.error(err);
     }
