52f71d2462a0fdc0e1c68f48f32f609f0472b12f
vconcat <imskyee@gmail.com>
2021-02-08 12:50:17 +0800

    install-node-apps.js: full path for cwd

--- install-node-apps.js
+++ install-node-apps.js
@@ -1,4 +1,5 @@
 const { execSync } = require("child_process");
+const path = require("path");
 const pkg = require("./package.json");
 
 const nodeApps = pkg.apps || [];
@@ -6,7 +7,7 @@ const nodeApps = pkg.apps || [];
 function installNodeApps() {
     for (let i = 0; i < nodeApps.length; i++) {
         execSync("npm install", {
-            cwd: nodeApps[i],
+            cwd: path.join(__dirname, nodeApps[i]),
             stdio: "inherit",
         });
     }
