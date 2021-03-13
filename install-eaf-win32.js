c06ea5363007231f1450cc7b1a2550e09860bab3
xhcoding <30917129+xhcoding@users.noreply.github.com>
2021-03-13 16:07:00 +0800

    Test whether qtwebengine works after installing dependencies (#610)

--- install-eaf-win32.js
+++ install-eaf-win32.js
@@ -1,4 +1,4 @@
-const { execSync } = require("child_process");
+const { spawn, execSync } = require("child_process");
 const path = require("path");
 const http = require("http");
 const https = require("https");
@@ -8,6 +8,36 @@ const os = require("os");
 const url = require("url");
 const process = require("process");
 
+async function execCmd(cmd, args) {
+    return new Promise((resolve, reject) => {
+        const res = spawn(cmd, args);
+        let output = "";
+        let err = "";
+
+        res.stdout.on("data", function(data) {
+            process.stdout.write(data);
+            const dataStr = data.toString();
+            output += dataStr;
+        });
+
+        res.stderr.on("data", function(data) {
+            process.stderr.write(data);
+            const errStr = data.toString();
+            err += errStr;
+        });
+
+        res.on("close", function(code) {
+            if (code === 0) {
+                resolve(output);
+            } else {
+                reject(err);
+            }
+        });
+        res.on("error", (err) => {
+            reject(err);
+        });
+    });
+}
 
 function printProgress(progress) {
     process.stdout.clearLine();
@@ -28,66 +58,109 @@ async function downloadFile(downloadUrlStr, sha256hash) {
     const filepath = path.join(os.tmpdir(), filename);
 
     return new Promise((resolve, reject) => {
-
         fs.open(filepath, "w", (err, fd) => {
             if (err) {
                 reject(err);
                 return;
             }
-            downloader.get(downloadUrl, (res) => {
-                const totalSize = res.headers["content-length"];
-                let downloadedSize = 0;
-                let progress = 0;
+            downloader
+                .get(downloadUrl, (res) => {
+                    const totalSize = res.headers["content-length"];
+                    let downloadedSize = 0;
+                    let progress = 0;
 
-                res.on("data", (chunk) => {
-                    fs.write(fd, chunk, (err) => {
-                        if (err) {
-                            reject(err);
-                            return;
-                        }
+                    res.on("data", (chunk) => {
+                        fs.write(fd, chunk, (err) => {
+                            if (err) {
+                                reject(err);
+                                return;
+                            }
+                        });
+
+                        hasher.update(chunk);
+
+                        downloadedSize += chunk.length;
+                        progress = ((downloadedSize / totalSize) * 100).toFixed(2);
+                        printProgress(
+                            `Downloading ${filename}, Downloaded: ${progress}%  `
+                        );
                     });
 
-                    hasher.update(chunk);
-
-                    downloadedSize += chunk.length;
-                    progress = (downloadedSize / totalSize * 100).toFixed(2);
-                    printProgress(`Downloading ${filename}, Downloaded: ${progress}%  `);
-                });
-
-                res.on("end", () => {
-                    const hash = hasher.digest("hex");
-                    if (hash !== sha256hash) {
-                        reject(new Error("download failed: hash mismatch"));
-                        return;
-                    }
-
-                    fs.close(fd, (err) => {
-                        if (err) {
-                            reject(err);
+                    res.on("end", () => {
+                        const hash = hasher.digest("hex");
+                        if (hash !== sha256hash) {
+                            reject(new Error("download failed: hash mismatch"));
                             return;
                         }
-                        console.info(`\nDownload ${filename} successed!`);
-                        resolve(filepath);
+
+                        fs.close(fd, (err) => {
+                            if (err) {
+                                reject(err);
+                                return;
+                            }
+                            console.info(`\nDownload ${filename} successed!`);
+                            resolve(filepath);
+                        });
                     });
+                })
+                .on("error", (err) => {
+                    reject(err);
                 });
-            }).on("error", (err) => {
-                reject(err);
-            });
         });
     });
 }
 
-
-function installCoreDeps() {
-    execSync("npm install", { stdio: "inherit" });
-    execSync("pip install -r requirements.txt", { stdio: "inherit" });
+async function installCoreDeps() {
+    await execCmd("npm.cmd", ["install"]);
+    await execCmd("pip3", ["install", "-r", "requirements.txt"]);
 }
 
+async function checkQtWebEngine() {
+    const pythonScript =
+        '\
+import sys\n\
+from PyQt5.QtCore import QUrl\n\
+from PyQt5.QtWebEngineWidgets import QWebEngineView\n\
+from PyQt5.QtWidgets import QApplication\n\
+app = QApplication(sys.argv)\n\
+view = QWebEngineView()\n\
+view.setUrl(QUrl("http://127.0.0.1:12345"))\n\
+view.resize(1024, 750)\n\
+view.show()\n\
+app.exec()\n\
+';
+
+    const tempPythonFilePath = path.join(
+        os.tmpdir(),
+        "install_eaf_win32_test.py"
+    );
+    const fd = fs.openSync(tempPythonFilePath, "w");
+    fs.writeSync(fd, pythonScript);
+    fs.closeSync(fd);
+
+    const requestListener = function(_req, res) {
+        res.writeHead(200);
+        res.end(
+            "<h1 style='color: green'>QtWebEngine is work! Close the window to continue the installation process</h1>"
+        );
+    };
+    const server = http.createServer(requestListener);
+    server.listen(12345);
+    try {
+        await execCmd("python", [tempPythonFilePath]);
+        server.close();
+    } catch (err) {
+        server.close();
+        throw err;
+    }
+}
 
 function checkCodecPack() {
     try {
-        execSync("REG QUERY HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\KLiteCodecPack_is1",
-            { stdio: "ignore" });
+        execSync(
+            "REG QUERY HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\KLiteCodecPack_is1",
+            { stdio: "ignore" }
+        );
         return true;
     } catch (e) {
         return false;
@@ -95,8 +168,10 @@ function checkCodecPack() {
 }
 
 async function installCodecPack() {
-    const kliteCodecPackURL = "https://files3.codecguide.com/K-Lite_Codec_Pack_1605_Basic.exe";
-    const kliteCodecPackHash = "1f2b2593c6de1f4f3724df673c2cf1d31c4a416355b4a64f532a52a825e4694a";
+    const kliteCodecPackURL =
+        "https://files3.codecguide.com/K-Lite_Codec_Pack_1605_Basic.exe";
+    const kliteCodecPackHash =
+        "1f2b2593c6de1f4f3724df673c2cf1d31c4a416355b4a64f532a52a825e4694a";
 
     if (checkCodecPack()) {
         console.info("k-lite-codec-pack is installed!");
@@ -109,18 +184,16 @@ async function installCodecPack() {
     console.info("install k-lite-codec-pack basic finished");
 }
 
-try {
-    installCoreDeps();
-    console.info('\x1b[32m%s\x1b[0m', "\n\nCore dependency install finished! You can use CTRL+C to terminal the following steps and enjoy EAF!\n\n");
-} catch (err) {
-    console.error(err);
-}
-
-
-
 (async function() {
     try {
+        await installCoreDeps();
+
+        await checkQtWebEngine();
+
+        console.info('\x1b[32m%s\x1b[0m', "\n\nCore dependency install finished! You can use CTRL+C to terminal the following steps and enjoy EAF!\n\n");
+
         await installCodecPack();
+
     } catch (err) {
         console.error(err);
     }
