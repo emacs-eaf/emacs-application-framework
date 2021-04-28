921cf977d70d5a83783cb1018e2607b9baefb843
Lucius Hu <lebensterben@users.noreply.github.com>
2021-04-28 00:29:24 -0400

    POSIX shell compatibility

--- install-eaf.sh
+++ install-eaf.sh
@@ -1,53 +1,56 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 set -eu
 
-ARCH_PACKAGES=(git nodejs npm)
-ARCH_PACKAGES+=(python-pyqt5 python-pyqt5-sip python-pyqtwebengine wmctrl)
-ARCH_PACKAGES+=(python-qrcode aria2 python-qtconsole)
+ARCH_PACKAGES=$(git nodejs npm)
+ARCH_PACKAGES="$ARCH_PACKAGES python-pyqt5 python-pyqt5-sip python-pyqtwebengine wmctrl \
+python-qrcode aria2 python-qtconsole"
 
 # System dependencies
-if apt -v &> /dev/null; then
-    sudo apt -y install git nodejs npm aria2 wmctrl
-    sudo apt -y install libglib2.0-dev
+if [ "$(command -v apt)" ]; then
     # Missing in Ubuntu: filebrowser-bin
+    # shellcheck disable=SC2015
+    sudo apt -y install git nodejs npm aria2 wmctrl &&
+        sudo apt -y install libglib2.0-dev &&
+        sudo apt -y install python3-pyqt5 python3-sip python3-pyqt5.qtwebengine \
+             python3-qrcode python3-feedparser \
+             python3-markdown python3-qtconsole python3-pygit2 ||
+            { echo "Failed to install dependency with apt."; exit 1;}
 
-    sudo apt -y install python3-pyqt5 python3-sip python3-pyqt5.qtwebengine \
-         python3-qrcode python3-feedparser \
-         python3-markdown python3-qtconsole python3-pygit2
-
-elif dnf &> /dev/null; then
-    sudo dnf -y install git nodejs npm aria2 wmctrl
-    sudo dnf -y install glib2-devel
+elif [ "$(command -v dnf)" ]; then
     # TODO: please add filebrowser-bin if it exists in Fedora repo.
+    # shellcheck disable=SC2015
+    sudo dnf -y install git nodejs npm aria2 wmctrl &&
+        sudo dnf -y install glib2-devel &&
+        sudo dnf -y install python-pyqt5 python3-pyqt5-sip pyqtwebengine-devel \
+             python3-qrcode python3-feedparser python3-markdown \
+             python3-qtconsole python3-pygit2 ||
+            { echo "Failed to install dependencies with dnf."; exit 1; }
 
-    sudo dnf -y install python-pyqt5 python3-pyqt5-sip pyqtwebengine-devel \
-         python3-qrcode python3-feedparser python3-markdown \
-         python3-qtconsole python3-pygit2
-
-elif type pacman &> /dev/null; then
-    sudo pacman -Sy --noconfirm --needed "${ARCH_PACKAGES[@]}"
-    if type yay &> /dev/null; then
-        yay -S --noconfirm filebrowser-bin
-    fi
+elif [ "$(command -v pacman)" ]; then
+    # shellcheck disable=SC2086
+    sudo pacman -Sy --noconfirm --needed $ARCH_PACKAGES ||
+        { echo "Failed to install dependency with pacman."; exit 1;}
+    [ "$(command -v yay)" ] && yay -S --noconfirm filebrowser-bin
 else
     echo "Unsupported distribution/package manager. Here are the packages that needs to be installed:"
-    for PCK in "${ARCH_PACKAGES[@]}";
-    do
+    for PCK in $ARCH_PACKAGES; do
+        # shellcheck disable=SC2039
         echo "- ${PCK}"
     done
-    echo "Please test their installation and submit an issue/PR to https://github.com/manateelazycat/emacs-application-framework for the script to be updated."
+    echo "Please test their installation and submit an issue/PR to \
+https://github.com/manateelazycat/emacs-application-framework for the script to be updated."
     exit 1
 fi
 
 echo "Installing npm dependencies..."
-npm install
+npm install || { echo "Failed to install dependency with npm."; exit 1;}
 
 # Python dependencies
-if type pip3 &>/dev/null; then
-    pip3 install --user pymupdf epc retrying
-elif type pip &>/dev/null; then
-    pip install --user pymupdf epc retrying
+if [ "$(command -v pip3)" ]; then
+    pip3 install --user pymupdf epc retrying || { echo "Failed to install dependency with pip3."; exit 1;}
+elif [ "$(command -v pip)" ]; then
+    pip install --user pymupdf epc retrying || { echo "Failed to install dependency with pip."; exit 1;}
 else
     echo "Cannot find pip. Please install it before launching the script again."
     exit 1
