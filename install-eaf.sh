a9b2613998861935bd0f9493226fa96381a7773f
Lucius Hu <1222865+lebensterben@users.noreply.github.com>
2021-04-30 18:20:15 +0000

    fixup! #+5

--- install-eaf.sh
+++ install-eaf.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-ARCH_PACKAGES=(git nodejs npm)
-ARCH_PACKAGES="$ARCH_PACKAGES python-pyqt5 python-pyqt5-sip python-pyqtwebengine wmctrl \
+ARCH_PACKAGES="git nodejs npm \
+python-pyqt5 python-pyqt5-sip python-pyqtwebengine wmctrl \
 python-qrcode aria2 python-qtconsole"
 
 # System dependencies
