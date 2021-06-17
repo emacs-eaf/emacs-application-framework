emacs-eaf/emacs-application-framework

commit eb72ceca7200a52bfabdd053d9d1fb9f675940f0

start epc server when needed (#698)

eaf.el
@@ -240,7 +240,7 @@ been initialized."
           epcs:server-processes)
     main-process))
 
-(defvar eaf-server
+(defun eaf--start-epc-server ()
   (let (server)
     (setq server (epcs:server-start
                   (lambda (mngr)
@@ -259,9 +259,10 @@ been initialized."
                   ))
     (if server
         (setq eaf-server-port (process-contact server :service))
-      (message "eaf-server fails to start.")
+      (error "eaf-server fails to start.")
       )
-    server))
+    server)
+  )
 
 (when noninteractive
   ;; Start "event loop".
@@ -1182,6 +1183,8 @@ A hashtable, key is url and value is title.")
     (user-error "[EAF] Please initiate EAF with eaf-open-... functions only"))
    ((epc:live-p eaf-epc-process)
     (user-error "[EAF] Process is already running")))
+  ;; start epc server and set `eaf-server-port'
+  (eaf--start-epc-server)
   (let* ((eaf-args (append
                     (list eaf-python-file)
                     (eaf-get-render-size)

commit 38d79eca501bf446d4c5f0b096d370bc63a6bb88

fix a problem introduced in pr #698 (#699)
* start epc server when needed

* fix the problem of creating multiple epc servers.

eaf.el
@@ -240,9 +240,10 @@ been initialized."
           epcs:server-processes)
     main-process))
 
+(defvar eaf-server nil)
 (defun eaf--start-epc-server ()
-  (let (server)
-    (setq server (epcs:server-start
+  (unless eaf-server
+    (setq eaf-server (epcs:server-start
                   (lambda (mngr)
                     (let ((mngr mngr))
                       (epc:define-method
@@ -257,11 +258,12 @@ been initialized."
                                             (read (substring arg 1)))
                                            (t arg)))) (cdr args)))))))
                   ))
-    (if server
-        (setq eaf-server-port (process-contact server :service))
+    (if eaf-server
+        (setq eaf-server-port (process-contact eaf-server :service))
       (error "eaf-server fails to start.")
       )
-    server)
+    )
+  eaf-server
   )
 
 (when noninteractive