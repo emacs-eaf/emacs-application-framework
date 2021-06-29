emacs-eaf/emacs-application-framework

commit e162955dcebca8495e1a0c4c707345f3ff55713d

Add convert_index_html function to fix ERR_FILE_NOT_FOUND errors.

core/webengine.py
@@ -113,8 +113,8 @@ class BrowserView(QWebEngineView):
          """ % (name)
         if immediately:
             self.web_page.runJavaScript(SCRIPT, QWebEngineScript.ApplicationWorld)
-        script = self.web_page.scripts().findScript(name)
-        self.web_page.scripts().remove(script)
+            script = self.web_page.scripts().findScript(name)
+            self.web_page.scripts().remove(script)
 
     def read_js_content(self, js_file):
         ''' Read content of JavaScript(js) files.'''
@@ -660,14 +660,14 @@ class BrowserBuffer(Buffer):
             font_family = self.emacs_var_dict[ 'eaf-browser-font-family']
             if font_family:
                 for ff in (
-                    self.settings.StandardFont,
-                    self.settings.FixedFont,
-                    self.settings.SerifFont,
-                    self.settings.SansSerifFont,
-                    # What's these font families?
-                    # self.settings.CursiveFont,
-                    # self.settings.FantasyFont,
-                    # self.settings.PictographFont
+                        self.settings.StandardFont,
+                        self.settings.FixedFont,
+                        self.settings.SerifFont,
+                        self.settings.SansSerifFont,
+                        # What's these font families?
+                        # self.settings.CursiveFont,
+                        # self.settings.FantasyFont,
+                        # self.settings.PictographFont
                 ):
                     self.settings.setFontFamily(ff, font_family)
         except Exception:
@@ -676,9 +676,8 @@ class BrowserBuffer(Buffer):
         self.build_all_methods(self.buffer_widget)
         self.build_all_methods(self)
 
-        # Reset to default zoom when page init or page url changed.
-        self.reset_default_zoom()
-        self.buffer_widget.urlChanged.connect(lambda url: self.reset_default_zoom())
+        # Reset with HiDPI.
+        self.buffer_widget.zoom_reset()
 
         # Build webchannel object.
         self.channel = QWebChannel()
@@ -884,7 +883,7 @@ class BrowserBuffer(Buffer):
                 self.buffer_widget.eval_js("CaretBrowsing.setInitialCursor();")
                 message_to_emacs("Caret browsing activated.")
                 self.caret_browsing_mode = True
-            eval_in_emacs('eaf--toggle-caret-browsing', ["'t" if self.caret_browsing_mode else "'nil"])
+                eval_in_emacs('eaf--toggle-caret-browsing', ["'t" if self.caret_browsing_mode else "'nil"])
 
     def caret_exit(self):
         ''' Exit caret browsing.'''
@@ -1078,7 +1077,7 @@ class BrowserBuffer(Buffer):
             zoom_factor = float(self.emacs_var_dict["eaf-browser-default-zoom"])
             for row in result:
                 zoom_factor = float(row[0])
-            self.buffer_widget.setZoomFactor(zoom_factor)
+                self.buffer_widget.setZoomFactor(zoom_factor)
 
     def atomic_edit(self):
         ''' Edit the focus text.'''
@@ -1205,6 +1204,18 @@ class BrowserBuffer(Buffer):
 
         setattr(self, python_method_name, _do)
 
+    def convert_index_html(self, index_file_content, dist_dir):
+        '''
+        Convert path to absolute path and change body background.
+        '''
+        return index_file_content.replace(
+            '''<link href=''', '''<link href=''' + dist_dir).replace(
+                '''<script src=''', '''<script src=''' + dist_dir).replace(
+                    '''<body>''', '''<body style="background: {}; color: {}">'''.format(
+                        self.emacs_var_dict["eaf-emacs-theme-background-color"],
+                        self.emacs_var_dict["eaf-emacs-theme-foreground-color"]
+                    ))
+
 class ZoomSizeDb(object):
     def __init__(self, dbpath):
         self._conn = sqlite3.connect(dbpath)
@@ -1228,7 +1239,7 @@ class ZoomSizeDb(object):
             INSERT INTO ZoomSize (Host, ZoomScale)
             VALUES (?, ?)
             """, (host, zoom_scale))
-        self._conn.commit()
+            self._conn.commit()
 
     def get_entry(self, host):
         return self._conn.execute("""

app/browser/buffer.py
@@ -104,6 +104,10 @@ class AppBuffer(BrowserBuffer):
         self.caret_foreground_color = QColor(self.emacs_var_dict["eaf-emacs-theme-background-color"])
         self.caret_background_color = QColor(self.emacs_var_dict["eaf-emacs-theme-foreground-color"])
 
+        # Reset to default zoom when page init or page url changed.
+        self.reset_default_zoom()
+        self.buffer_widget.urlChanged.connect(lambda url: self.reset_default_zoom())
+
         # Reset zoom after page loading finish.
         # Otherwise page won't zoom if we call setUrl api in current page.
         self.buffer_widget.loadFinished.connect(lambda : self.buffer_widget.zoom_reset())

app/music-player/buffer.py
@@ -45,11 +45,7 @@ class AppBuffer(BrowserBuffer):
         self.buffer_widget.loadFinished.connect(self.load_first_file)
 
         with open(self.index_file, "r") as f:
-            html = f.read().replace(
-                '''href="''', '''href="''' + self.index_file_dir).replace(
-                    '''<script src="''', '''<script src="''' + self.index_file_dir).replace(
-                        '''<body>''', '''<body style="background: {}">'''.format(self.emacs_var_dict["eaf-emacs-theme-background-color"])
-                    )
+            html = self.convert_index_html(f.read(), self.index_file_dir)
             self.buffer_widget.setHtml(html, QUrl("file://"))
 
         for (python_method_name, js_method_name) in [("play_next", "playNextItem"),

app/system-monitor/buffer.py
@@ -42,11 +42,7 @@ class AppBuffer(BrowserBuffer):
         self.buffer_widget.loadFinished.connect(self.load_first_file)
 
         with open(self.index_file, "r") as f:
-            html = f.read().replace(
-                '''href="''', '''href="''' + self.index_file_dir).replace(
-                    '''<script src="''', '''<script src="''' + self.index_file_dir).replace(
-                        '''<body>''', '''<body style="background: {}">'''.format(self.emacs_var_dict["eaf-emacs-theme-background-color"])
-                    )
+            html = self.convert_index_html(f.read(), self.index_file_dir)
             self.buffer_widget.setHtml(html, QUrl("file://"))
 
         for (python_method_name, js_method_name) in [("scroll_up", "scrollUp"),

app/vue-demo/buffer.py
@@ -32,5 +32,5 @@ class AppBuffer(BrowserBuffer):
         self.url = url
 
         with open(self.index_file, "r") as f:
-            html = f.read().replace('''href="''', '''href="''' + self.index_file_dir).replace('''<script src="''', '''<script src="''' + self.index_file_dir)
+            html = self.convert_index_html(f.read(), self.index_file_dir)
             self.buffer_widget.setHtml(html, QUrl("file://"))

commit 602fad2fe1c6c3a198c7867502c4b51be17ef1e3

Fix browser caret bug caused by commit e162955
* Bug description:
Once we get into `browse-caret-mode`, then we can't quit this mode.
Specifically, we can't use j and k to scroll up and down when we can't
quit this mode.

* Other words:
Actually, this is a very simple bug, which is merely caused by
mistaken indent when manateelazycat solve other issues.

And I noticed there are other changes on indent, such as at line 116,
1080 and 1242 of `webengine.py` file in commit e162955. but I can't
understand these lines. So I didn't changes these lines. I just want
to notice if there are something messed up too!

By the way, this is my first time to do a PR, and I am not good at
english, So if there are some nonstandard expressions and wrong
english grammar, I'm very sorry about that!

core/webengine.py
@@ -883,7 +883,7 @@ class BrowserBuffer(Buffer):
                 self.buffer_widget.eval_js("CaretBrowsing.setInitialCursor();")
                 message_to_emacs("Caret browsing activated.")
                 self.caret_browsing_mode = True
-                eval_in_emacs('eaf--toggle-caret-browsing', ["'t" if self.caret_browsing_mode else "'nil"])
+            eval_in_emacs('eaf--toggle-caret-browsing', ["'t" if self.caret_browsing_mode else "'nil"])
 
     def caret_exit(self):
         ''' Exit caret browsing.'''