2d8ae17b76248cc4d8aabcaba93af31bbdb56214
Mingde (Matthew) Zeng <matthewzmd@posteo.net>
2021-07-23 01:46:01 -0400

    Improve log of install-* scripts
    Signed-off-by: Mingde (Matthew) Zeng <matthewzmd@posteo.net>

--- install-eaf.sh
+++ install-eaf.sh
@@ -25,7 +25,7 @@ if [ "$(command -v apt)" ]; then
         sudo apt install -y python3-pyqt5 python3-sip python3-pyqt5.qtwebengine \
              python3-qrcode python3-feedparser \
              python3-markdown python3-qtconsole python3-pygit2 libtag1-dev ||
-            { echo "Failed to install dependency with apt."; exit 1;}
+            { echo "[EAF] Failed to install dependency with apt."; exit 1;}
 
 elif [ "$(command -v dnf)" ]; then
     # TODO: please add filebrowser-bin if it exists in Fedora repo.
@@ -35,15 +35,15 @@ elif [ "$(command -v dnf)" ]; then
         sudo dnf -y install python3-qt5 python3-pyqt5-sip pyqtwebengine-devel \
              python3-qrcode python3-feedparser python3-markdown \
              python3-qtconsole python3-pygit2 taglib-devel ||
-            { echo "Failed to install dependencies with dnf."; exit 1; }
+            { echo "[EAF] Failed to install dependencies with dnf."; exit 1; }
 
 elif [ "$(command -v pacman)" ]; then
     # shellcheck disable=SC2086
     sudo pacman -Sy --noconfirm --needed $ARCH_PACKAGES ||
-        { echo "Failed to install dependency with pacman."; exit 1;}
+        { echo "[EAF] Failed to install dependency with pacman."; exit 1;}
     [ "$(command -v yay)" ] && yay -S --noconfirm filebrowser-bin
 else
-    echo "Unsupported distribution/package manager. Here are the packages that needs to be installed:"
+    echo "[EAF] Unsupported distribution/package manager. Here are the packages that needs to be installed:"
     for PCK in $ARCH_PACKAGES; do
         # shellcheck disable=SC2039
         echo "- ${PCK}"
@@ -53,17 +53,17 @@ https://github.com/manateelazycat/emacs-application-framework for the script to 
     exit 1
 fi
 
-echo "Installing npm dependencies..."
-npm install || { echo "Failed to install dependency with npm."; exit 1;}
-
 # Python dependencies
 if [ "$(command -v pip3)" ]; then
-    pip3 install --user pymupdf epc retrying pytaglib psutil || { echo "Failed to install dependency with pip3."; exit 1;}
+    pip3 install --user pymupdf epc retrying pytaglib psutil || { echo "[EAF] Failed to install dependency with pip3."; exit 1;}
 elif [ "$(command -v pip)" ]; then
-    pip install --user pymupdf epc retrying pytaglib psutil || { echo "Failed to install dependency with pip."; exit 1;}
+    pip install --user pymupdf epc retrying pytaglib psutil || { echo "[EAF] Failed to install dependency with pip."; exit 1;}
 else
-    echo "Cannot find pip. Please install it before launching the script again."
+    echo "[EAF] Cannot find pip. Please install it before launching the script again."
     exit 1
 fi
 
-echo "eaf-install.sh finished."
+echo "[EAF] Installing npm dependencies..."
+npm install || { echo "[EAF] Failed to install dependency with npm."; exit 1;}
+
+echo "[EAF] eaf-install.sh finished."
