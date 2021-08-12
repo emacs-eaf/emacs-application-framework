9c572bfd90a420e3d9e662e016091c9b2c4a1ec2
Andy Stewart <lazycat.manatee@gmail.com>
2021-08-12 19:55:48 +0800

    Remove update-eaf.py

--- update-eaf.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import subprocess
-
-subprocess.Popen(["git", "pull"], stdin = subprocess.PIPE, text=True).wait()
-subprocess.Popen(["python3", "install-eaf.py"], stdin = subprocess.PIPE, text=True).wait()
