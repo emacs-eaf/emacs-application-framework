b882b137faea4edc26148164c8dc8d6f60512fec
Andy Stewart <lazycat.manatee@gmail.com>
2021-08-12 18:37:26 +0800

    Use python3 instead python.

--- update-eaf.py
+++ update-eaf.py
@@ -4,4 +4,4 @@
 import subprocess
 
 subprocess.Popen(["git", "pull"], stdin = subprocess.PIPE, text=True).wait()
-subprocess.Popen(["python", "install-eaf.py"], stdin = subprocess.PIPE, text=True).wait()
+subprocess.Popen(["python3", "install-eaf.py"], stdin = subprocess.PIPE, text=True).wait()
