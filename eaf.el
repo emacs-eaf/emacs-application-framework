821a2ebe21ef608a9960544b626168559d07008c
Thibaut Verron <thibaut.verron@gmail.com>
2021-09-20 13:17:51 +0200

    Boilerplate for version testing and automatic install

--- eaf.el
+++ eaf.el
@@ -370,6 +370,9 @@ been initialized."
   "Name of EAF buffer."
   :type 'string)
 
+(defcustom eaf-apps nil
+  "List of applications to install")
+
 (defcustom eaf-python-command (if (memq system-type '(cygwin windows-nt ms-dos)) "python.exe" "python3")
   "The Python interpreter used to run eaf.py."
   :type 'string)
@@ -1641,6 +1644,26 @@ It currently identifies PDF, videos, images, and mindmap file extensions."
       (eaf-call-async "handle_search_backward" eaf-search-input-buffer-id eaf-search-input-callback-tag)
     (apply orig-fun arg args)))
 (advice-add #'isearch-backward :around #'eaf--isearch-backward-advisor)
+
+;; Automatic installation
+(defvar eaf-version-file "eaf-version-file.txt")
+
+(defvar eaf-build-dir (file-name-directory (locate-library "eaf")))
+(defvar eaf-source-dir (file-name-directory (file-truename (concat eaf-build-dir "eaf.el"))))
+
+(defvar eaf-commit (let ((default-directory eaf-source-dir)) (shell-command-to-string "git rev-parse HEAD" )))
+
+(defcustom eaf-force-compile nil)
+
+(unless
+    (and (not eaf-force-compile)
+	 (string= eaf-commit
+		  (with-temp-buffer
+		    (insert-file-contents eaf-version-file)
+		    (buffer-string))))
+  (eaf-install-and-update)
+  (with-temp-file eaf-version-file
+    (insert eaf-commit)))
 
 (provide 'eaf)
 
