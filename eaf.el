bba64c7755eda0d81f3b9fb7d4996ed83dc9420c
Andy Stewart <lazycat.manatee@gmail.com>
2022-03-01 17:48:21 +0800

    Re-implement eaf-add-subdirs-to-load-path.

--- eaf.el
+++ eaf.el
@@ -113,56 +113,28 @@ A bookmark handler function is used as
 A new app can use this to configure extensions which should
 handled by it.")
 
-(defun eaf-add-subdirs-to-load-path ()
-  "Recursively add all subdirectories of `default-directory' to `load-path'.
-More precisely, this uses only the subdirectories whose names
-start with letters or digits; it excludes any subdirectory named `RCS'
-or `CVS', and any subdirectory that contains a file named `.nosearch'."
-  (let (dirs
-        attrs
-        (pending (list (file-name-directory (locate-library "eaf")))))
-    ;; This loop does a breadth-first tree walk on DIR's subtree,
-    ;; putting each subdir into DIRS as its contents are examined.
-    (while pending
-      (push (pop pending) dirs)
-      (let* ((this-dir (car dirs))
-             (contents (directory-files this-dir))
-             (default-directory this-dir)
-             (canonicalized (if (fboundp 'w32-untranslated-canonical-name)
-                                (w32-untranslated-canonical-name this-dir))))
-        ;; The Windows version doesn't report meaningful inode numbers, so
-        ;; use the canonicalized absolute file name of the directory instead.
-        (setq attrs (or canonicalized
-                        (nthcdr 10 (file-attributes this-dir))))
-        (unless (member attrs normal-top-level-add-subdirs-inode-list)
-          (push attrs normal-top-level-add-subdirs-inode-list)
-          (dolist (file contents)
-            (and
-             ;; NOTE:
-             ;; Don't scan node_modules directories, such as EAF npm subdirectories.
-             (not (string-match-p "/node_modules" this-dir))
-             (not (string-match-p "/dist" this-dir))
+(defun eaf-add-subdirs-to-load-path (search-dir)
+  (interactive)
+  (let* ((dir (file-name-as-directory search-dir)))
+    (dolist (subdir (cl-remove-if
+                     #'(lambda (subdir)
+                         (or (member subdir '("." ".." "dist" "node_modules" "RCS" "CVS" "rcs" "cvs" "__pycache__" ".git" ".gitattributes" ".gitignore" ".gitmodules"))
+                             (not (file-directory-p (concat dir subdir)))))
+                     (directory-files dir)))
+      (let ((subdir-path (concat dir (file-name-as-directory subdir))))
+        (when (cl-some #'(lambda (subdir-file)
+                           (and (file-regular-p (concat subdir-path subdir-file))
+                                (member (file-name-extension subdir-file) '("el" "so" "dll"))))
+                       (directory-files subdir-path))
+          ;; NOTE:
+          ;; We should add path to `load-path' by append, otherwise Emacs can't load expected.
+          (add-to-list 'load-path subdir-path t))
 
-             (string-match "\\`[[:alnum:]]" file)
-             ;; The lower-case variants of RCS and CVS are for DOS/Windows.
-             (not (member file '("RCS" "CVS" "rcs" "cvs")))
-             (file-directory-p file)
-             (let ((expanded (expand-file-name file)))
-               (or (file-exists-p (expand-file-name ".nosearch" expanded))
-                   (setq pending (nconc pending (list expanded))))))))))
-    (normal-top-level-add-to-load-path (cdr (nreverse dirs)))))
+        ;; Search subdirectories recursively.
+        (add-subdirs-to-load-path subdir-path)))))
 
-(defun eaf-add-app-dirs-to-load-path ()
-  "Add EAF app directories where .el exists to `load-path'."
-  (let ((default-directory (file-name-directory (locate-library "eaf"))))
-    (add-to-list 'load-path default-directory)
-    (eaf-add-subdirs-to-load-path)
-    (dolist (path load-path)
-      (when (or (string-match-p "/node_modules" path)
-                (string-match-p "/dist" path))
-        (setq load-path (delete path load-path))))))
-
-(eaf-add-app-dirs-to-load-path)
+;; Add EAF app directories where .el exists to `load-path'.
+(eaf-add-subdirs-to-load-path (file-name-directory (locate-library "eaf")))
 
 (require 'eaf-epc)
 
