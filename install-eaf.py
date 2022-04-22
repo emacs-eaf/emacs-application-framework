29e61b16c16554f90f2e1f760e74cdfa8a5995da
Andy Stewart <lazycat.manatee@gmail.com>
2022-04-22 14:37:33 +0800

    Use PyQt6 6.2.0 won't crash browser when type return/backspace key.

--- install-eaf.py
+++ install-eaf.py
@@ -272,14 +272,8 @@ def install_core_deps(distro, deps_dict):
         if len(core_deps) > 0:
             install_sys_deps(distro, core_deps)
     if (not args.ignore_py_deps or sys.platform != "linux") and sys.platform in deps_dict["pip"]:
-        if distro in deps_dict["pip"]:
-            # The priority of the distribution is higher than "linux".
-            install_py_deps(deps_dict["pip"][distro])
-        else:
-            install_py_deps(deps_dict["pip"][sys.platform])
+        install_py_deps(deps_dict["pip"][sys.platform])
 
-    if sys.platform != "win32" and sys.platform != "cygwin":
-        symlink_webengine_library(distro)
     print("[EAF] Finished installing core dependencies")
 
 def yes_no(question, default_yes=False, default_no=False):
@@ -404,37 +398,6 @@ def install_app_deps(distro, deps_dict):
     print("\n[EAF] Please ensure the following are added to your init.el:")
 
     print_sample_config(app_dir)
-
-def symlink_webengine_library(distro):
-    if distro == "pacman":
-        return
-    library_name = "libQt6WebEngineCore.so.6"
-    python_library_dir = site.getsitepackages()[0]
-    python_library_path = os.path.join(python_library_dir, "PyQt6", "Qt6", "lib", library_name)
-
-    process = subprocess.Popen(''' echo "$(ldconfig -p | grep {} | tr ' ' '\n' | grep /)" '''.format(library_name),
-                               shell=True, text=True,
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    process.wait()
-
-    webengine_library_path = ""
-    try:
-        webengine_library_path =  process.stdout.readlines()[0].split()[0]
-    except:
-        pass
-
-    if webengine_library_path == "":
-        important_messages.append("[EAF] Failed to identify {}, EAF Browser may crash".format(python_library_path))
-        important_messages.append("[EAF] To fix this problem, please install package that includes {} from your OS repository, then symlink {} to {}".format(
-            library_name, library_name, python_library_path))
-    else:
-        if os.path.getsize(webengine_library_path) != os.path.getsize(python_library_path):
-            print("[EAF] Fix EAF Browser crash cause by {}: sudo ln -sf {} {}".format(library_name, webengine_library_path, python_library_path))
-            symlink_process = subprocess.Popen("sudo ln -sf {} {}".format(webengine_library_path, python_library_path),
-                                               shell=True, text=True,
-                                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            symlink_process.wait()
-            print("[EAF] Finished {} symlink.".format(library_name))
 
 def main():
     try:
