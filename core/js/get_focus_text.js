77b543513d1970bd8f7320b45a50d6f38481163e
Andy Stewart <lazycat.manatee@gmail.com>
2022-08-23 17:06:29 +0800

    get_focus_text support input type 'email'

--- core/js/get_focus_text.js
+++ core/js/get_focus_text.js
@@ -1,28 +1,30 @@
 (function() {
-    const activeElement = document.activeElement;
-    var inputs = ["input", "select", "textarea"];
-    var pageUrl = window.location.href;
-    var tagName = activeElement.tagName.toLowerCase();
+  const activeElement = document.activeElement;
+  var inputs = ["input", "select", "textarea"];
+  var inputTypes = ["text", "textarea", "email"];
+  var pageUrl = window.location.href;
+  var tagName = activeElement.tagName.toLowerCase();
 
-    if (pageUrl === "https://mail.qq.com/" && activeElement) {
-        // QQ mail have some security mechanism that we can't fetch value of activeElement.
-        // So we just return empty string make is_focus method works well in browser.py
-        return "";
-    } else if (activeElement && 
-               inputs.indexOf(tagName) !== -1 && 
-               (activeElement.type == "text" || activeElement.type == "textarea")) {
-        return activeElement.value;
-    } else if (activeElement.isContentEditable) {
-        // For the Rich Text Editor
-        return activeElement.textContent;
+  if (pageUrl === "https://mail.qq.com/" && activeElement) {
+    // QQ mail have some security mechanism that we can't fetch value of activeElement.
+    // So we just return empty string make is_focus method works well in browser.py
+    return "";
+  } else if (activeElement &&
+             inputs.indexOf(tagName) !== -1 &&
+             inputTypes.indexOf(activeElement.type) !== -1
+             ) {
+    return activeElement.value;
+  } else if (activeElement.isContentEditable) {
+    // For the Rich Text Editor
+    return activeElement.textContent;
+  } else {
+    if (pageUrl.startsWith("https://web.telegram.org/") && activeElement.hasAttribute("placeholder")) {
+      return activeElement.textContent;
+    } else if ((pageUrl.startsWith("https://console.cloud.google.com/") || pageUrl.startsWith("https://ssh.cloud.google.com/")) && tagName == "iframe") {
+      // Make user can type text in Terminal of Google Cloud.
+      return "";
     } else {
-        if (pageUrl.startsWith("https://web.telegram.org/") && activeElement.hasAttribute("placeholder")) {
-            return activeElement.textContent;
-        } else if ((pageUrl.startsWith("https://console.cloud.google.com/") || pageUrl.startsWith("https://ssh.cloud.google.com/")) && tagName == "iframe") {
-            // Make user can type text in Terminal of Google Cloud.
-            return "";
-        } else {
-            return undefined;
-        }
+      return undefined;
     }
+  }
 })();
