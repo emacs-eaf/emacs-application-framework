0765e8f4d9bf2ee456ede0080d2e61d2233704c6
Albert.Zhou <albert748@gmail.com>
2024-03-13 21:05:04 +0800

    Add contentedit area focus & set text support, especially for gemini

--- core/js/set_focus_text.js
+++ core/js/set_focus_text.js
@@ -2,15 +2,20 @@
     let newText = "%{new_text_base64}";
     const activeElement = document.activeElement;
 
-    if (window.location.href.startsWith("https://web.telegram.org/")) {
-        activeElement.textContent = decodeURIComponent(escape(window.atob(newText)));
+    // Decode the base64 encoded text
+    const decodedText = decodeURIComponent(escape(window.atob(newText)));
+
+    // Check if the active element is a typical input or textarea
+    if ("value" in activeElement) {
+	activeElement.value = decodedText;
     } else {
-        activeElement.value = decodeURIComponent(escape(window.atob(newText)));
+	// For contenteditable elements or others (like divs for Telegram and Google Gemini),
+	// directly set textContent. This approach works uniformly across various types of elements.
+	activeElement.textContent = decodedText;
     }
 
-    // Note: simulate input event on active element after set focus text.
-    // Some website need input event before submit form.
-    var event = document.createEvent('Event');
-    event.initEvent('input', true, true);
+    // Simulate input event on the active element or a found contenteditable element
+    // after setting the text. Some websites need input event before form submission.
+    var event = new Event('input', { bubbles: true, cancelable: true });
     activeElement.dispatchEvent(event);
 })();
